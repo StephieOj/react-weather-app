c72aa562f7560235764f9106ff297a69aa58e951
stephieOj <quilamana@outlook.com>
2025-10-03 12:49:22 +0100

    Created footer with link to respo on Github

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 
+import React from 'react';
 import './App.css';
 
 function App() {
-  return ( <div className="App"> Weather App</div>);
+  return ( 
+  <div className="App"> 
+  Weather App
+  <footer> This app was coded by StephieOj 💕 and is open-sourced on <a className= "github-link" href="https://github.com/StephieOj/react-weather-app" target='_blank'>Github</a>.</footer>
+  
+  
+  </div>);
     
 }
 
