ca3cb8db27052513078d03303affda6ed96a907d
stephieOj <quilamana@outlook.com>
2025-10-03 13:36:31 +0100

    Added Bootstrap to project and updated footer

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
   return ( 
   <div className="App"> 
-  Weather App
+  <div className ="container">
+  <h1>Weather App</h1>
   <footer> This app was coded by StephieOj 💕 and is open-sourced on <a className= "github-link" href="https://github.com/StephieOj/react-weather-app" target='_blank' rel="noopener noreferrer">Github</a>.</footer>
-  
-  
+  </div>
   </div>);
     
 }
