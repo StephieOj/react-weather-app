653f979d87818d749769caa83d7525005492e917
stephieOj <quilamana@outlook.com>
2025-10-03 19:17:53 +0100

    applied styling to initial structure

--- src/Weather.js
+++ src/Weather.js
@@ -3,26 +3,35 @@ import './Weather.css';
 
 export default function Weather() {
   return (<div className="Weather">
+
     <form>
       <div className="row">
         <div className="col-9">
           <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
         </div>
+
         <div className="col-3">
-          <input type="submit" value="Search" className="btn btn-primary w-100" />
+          <input type="submit" value="Search" className="btn btn-secondary w-100 search-btn" />
         </div>
+        
       </div>
     </form>
+
     <h1>London</h1>
-  <ul>
-    <li>Monday 08:00</li>
-    <li>Mostly Cloudy</li>
-  </ul>
-  <div className ="row">
-  <div className="col-6">
+
+    <ul>
+      <li>Monday 08:00</li>
+      <li>Mostly Cloudy</li>
+    </ul>
+
+    <div className ="row">
+
+    <div className="col-6">
     <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-    6°C
-  </div>
+    <span className="temperature">6</span>  
+    <span className="unit">°C</span>
+    </div>
+
   <div className="col-6">
     <ul>
       <li>Precipitation: 10%</li>
@@ -30,6 +39,7 @@ export default function Weather() {
       <li>Wind: 10 km/h</li>
     </ul>
   </div>
+
   </div>
   </div>
   );
